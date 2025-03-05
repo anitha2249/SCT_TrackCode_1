890881ebb4b258e7db3eca5306951ba2617a27ec
anitha2249 <inapanurianitha22@gmail.com>
2025-03-05 22:52:24 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,23 @@
+
+function b1() {
+    document.getElementById("con").style.backgroundColor = "#e0e0e0";
+    document.getElementById("color").textContent = "#e0e0e0";
+
+}
+
+function b2() {
+    document.getElementById("con").style.backgroundColor = "#6fcf97";
+    document.getElementById("color").textContent = "#6fcf97";
+
+}
+
+function b3() {
+    document.getElementById("con").style.backgroundColor = "#56ccf2";
+    document.getElementById("color").textContent = "#56ccf2";
+
+}
+
+function b4() {
+    document.getElementById("con").style.backgroundColor = "#bb6bd9";
+    document.getElementById("color").textContent = "#bb6bd9";
+}
